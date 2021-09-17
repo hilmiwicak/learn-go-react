2aa8a60586fae3ebeded13715224ee03accf20f0
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-09-17 13:00:05 +0700

    added http listener go

--- go-server/api/main.go
+++ go-server/api/main.go
@@ -1,23 +1,54 @@
 package main
 
 import (
-    "flag";
-    "fmt"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 const version = "1.0.0"
 
 type config struct {
-    port int
-    env string
+	port int
+	env  string
 }
 
-func main(){
-    var cfg config
+type AppStatus struct {
+	Status      string `json:"status"`
+	Environment string `json:"environment"`
+	Version     string `json:"version"`
+}
 
-    flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
-    flag.StringVar(&cfg.env, "env", "dev", "Application Environment (dev|prod)")
-    flag.Parse()
+func main() {
+	var cfg config
 
-    fmt.Println("Running .... ")
+	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "dev", "Application Environment (dev|prod)")
+	flag.Parse()
+
+	fmt.Println("Running .... ")
+
+	http.HandleFunc("/status", func(writer http.ResponseWriter, request *http.Request) {
+		currentStatus := AppStatus{
+			Status:      "available",
+			Environment: cfg.env,
+			Version:     version,
+		}
+
+		json, err := json.MarshalIndent(currentStatus, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusOK)
+		writer.Write(json)
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", cfg.port), nil)
+	if err != nil {
+		log.Println(err)
+	}
 }
