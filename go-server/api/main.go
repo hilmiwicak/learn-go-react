f3f2e58e88abf1ce21a644fec48470a5a02a56ac
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-10-26 23:14:52 +0700

    renamed movieHandlers & statusHandler

--- go-server/api/main.go
+++ go-server/api/main.go
@@ -36,6 +36,21 @@ type application struct {
 	models models.Models
 }
 
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	var cfg config
 
@@ -72,19 +87,4 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-func openDB(cfg config) (*sql.DB, error) {
-	db, err := sql.Open("postgres", cfg.db.dsn)
-	if err != nil {
-		return nil, err
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	err = db.PingContext(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return db, nil
 }
