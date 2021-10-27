783215694be0257f70653d6a7e573b9d3053081f
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-10-27 11:38:36 +0700

    fetch movies from frontend

--- react/src/components/Movies.js
+++ react/src/components/Movies.js
@@ -4,31 +4,39 @@ import { Link } from "react-router-dom";
 export default class Movies extends React.Component {
   state = {
     movies: [],
+    isLoaded: false,
   };
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        { id: 1, title: "It's Kind of A Funny Story", runtime: 142 },
-        { id: 2, title: "Batman Rises (1)", runtime: 159 },
-        { id: 3, title: "Batman Rises (2)", runtime: 188 },
-      ],
-    });
+    fetch("http://localhost:4000/v1/movies")
+      .then((response) => response.json())
+      .then((json) =>
+        this.setState({
+          movies: json.movies,
+          isLoaded: true,
+        })
+      );
   }
 
   render() {
-    return (
-      <Fragment>
-        <h2>Choose a movie</h2>
+    const { movies, isLoaded } = this.state;
 
-        <ul>
-          {this.state.movies.map((movie) => (
-            <li key="movie.id">
-              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </Fragment>
-    );
+    if (!isLoaded) {
+      return (<p>Loading...</p>);
+    } else {
+      return (
+        <Fragment>
+          <h2>Choose a movie</h2>
+
+          <ul>
+            {movies.map((movie) => (
+              <li key="movie.id">
+                <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      );
+    }
   }
 }
