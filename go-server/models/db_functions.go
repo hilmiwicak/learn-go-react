2f3b65a03efc252d06979ec9506b2790ea9e7e6f
hilmiwicak <hilmiwicaksonop@gmail.com>
2021-10-27 09:03:23 +0700

    retrieved all movies & changed movieGenresQuery into a const

--- go-server/models/db_functions.go
+++ go-server/models/db_functions.go
@@ -11,22 +11,38 @@ type DBModel struct {
 }
 
 /*
+ * constant for MovieGenres / genres query
+ * the $1 is a placeholder
+ */
+const movieGenresQuery = `
+	SELECT
+		mg.id, mg.movie_id, mg.genre_id, g.genre_name
+	FROM
+		movies_genres AS mg
+	LEFT JOIN
+		genres AS g
+	ON
+		g.id = mg.genre_id
+	WHERE
+		mg.movie_id = $1
+`
+
+/*
  * returns one movie and error, if any
  */
 func (m *DBModel) Movie(id int) (*Movie, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	// the $1 is a placeholder
-	query := `	SELECT 
-					id, title, description, year, release_date, rating, runtime, mpaa_rating, created_at, updated_at 
-				FROM 
-					movies
-				WHERE
-					id = $1
+	movieQuery := `
+					SELECT 
+						id, title, description, year, release_date, rating, runtime, mpaa_rating, created_at, updated_at 
+					FROM 
+						movies
+					WHERE
+						id = $1
 	`
-
-	row := m.DB.QueryRowContext(ctx, query, id)
+	row := m.DB.QueryRowContext(ctx, movieQuery, id)
 
 	var movie Movie
 
@@ -46,19 +62,7 @@ func (m *DBModel) Movie(id int) (*Movie, error) {
 		return nil, err
 	}
 
-	query = `	SELECT
-					mg.id, mg.movie_id, mg.genre_id, g.genre_name
-				FROM
-					movies_genres AS mg
-				LEFT JOIN
-					genres AS g
-				ON
-					 g.id = mg.genre_id
-				WHERE
-					mg.movie_id = $1
-	`
-
-	rows, _ := m.DB.QueryContext(ctx, query, id)
+	rows, _ := m.DB.QueryContext(ctx, movieGenresQuery, id)
 	defer rows.Close()
 
 	genres := make(map[int]string)
@@ -85,6 +89,65 @@ func (m *DBModel) Movie(id int) (*Movie, error) {
 /*
  * returns all movies and error if any
  */
-func (m *DBModel) AllMovies(id int) ([]*Movie, error) {
-	return nil, nil
+func (m *DBModel) AllMovies() ([]*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `	SELECT 
+					id, title, description, year, release_date, rating, runtime, mpaa_rating, created_at, updated_at 
+				FROM 
+					movies
+				ORDER BY
+					title
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var movies []*Movie
+
+	for rows.Next() {
+		var movie Movie
+		err := rows.Scan(
+			&movie.ID,
+			&movie.Title,
+			&movie.Description,
+			&movie.Year,
+			&movie.ReleaseDate,
+			&movie.Rating,
+			&movie.Runtime,
+			&movie.MPAARating,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		genreRows, _ := m.DB.QueryContext(ctx, movieGenresQuery, movie.ID)
+
+		genres := make(map[int]string)
+		for genreRows.Next() {
+			var mg MovieGenre
+			err := genreRows.Scan(
+				&mg.ID,
+				&mg.MovieID,
+				&mg.GenreID,
+				&mg.Genre.GenreName,
+			)
+			if err != nil {
+				return nil, err
+			}
+			genres[mg.ID] = mg.Genre.GenreName
+		}
+		genreRows.Close()
+
+		movie.MovieGenre = genres
+		movies = append(movies, &movie)
+	}
+
+	return movies, nil
 }
